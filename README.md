65a109f9fd9037c1cb05249489682cd7ac327adf
StavKinrot <stav.kin1999@gmail.com>
2024-05-23 00:31:39 +0300

    update

---
+++ README.md
@@ -0,0 +1,4 @@
+# Crossy Sky
+Avoid the bad eagles and level up each time you successfully reach the top of the screen.
+It's gets harder with each level!
+
--- obstacles.py
+++ obstacles.py
@@ -16,11 +16,6 @@ class Obstacles(pygame.sprite.Sprite):
             fly_3 = pygame.image.load('graphics/Eagle/left3.png').convert_alpha()
             fly_3 = pygame.transform.rotozoom(fly_3, 0, 2)
             self.frames = [fly_1, fly_2, fly_3]
-        # else:
-        #     snail_1 = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-        #     snail_2 = pygame.image.load('graphics/snail/snail2.png').convert_alpha()
-        #     self.frames = [snail_1, snail_2]
-        #     y_pos = 300
 
         self.animation_index = 0
         self.image = self.frames[self.animation_index]
