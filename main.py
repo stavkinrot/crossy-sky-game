ab7cb7e9b60641e804da7903084dfd2f1d8e82a7
StavKinrot <stav.kin1999@gmail.com>
2024-05-22 13:14:52 +0300

    update

--- main.py
+++ main.py
@@ -22,10 +22,11 @@ sky_surface = pygame.transform.rotozoom(sky_surface, 0, 0.4)
 
 def display_score():
     score_cnt = 0  # Add functionallity
-    score_surf = test_font.render(f'Score: {score_cnt}', False, (64, 64, 64))
+    score_surf = test_font.render(f'Score: {score_cnt}', False, (225, 245, 255))
     score_rect = score_surf.get_rect(center=(400, 50))
     screen.blit(score_surf, score_rect)
     return score_cnt
+
 
 
 def collision_sprite():
@@ -43,15 +44,16 @@ player.add(Player())
 obstacle_group = pygame.sprite.Group()
 
 # Intro screen
-player_stand = pygame.image.load('graphics/player/player_stand.png').convert_alpha()
+player_stand = pygame.image.load('graphics/White Bird/left2.png').convert_alpha()
 player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
 player_stand_rect = player_stand.get_rect(center=(400, 400))
 
-game_name = test_font.render('Cross Sky', False, (111, 196, 169))
-game_name_rect = game_name.get_rect(center=(400, 80))
+game_name = test_font.render('Crossy Sky', False, (225, 245, 255))
+game_name = pygame.transform.rotozoom(game_name,0 , 2)
+game_name_rect = game_name.get_rect(center=(400, 200))
 
-game_message = test_font.render('Press UP to fly', False, (111, 196, 169))
-game_message_rect = game_message.get_rect(center=(400, 330))
+game_message = test_font.render('Press UP to fly', False, (225, 245, 255))
+game_message_rect = game_message.get_rect(center=(400, 600))
 
 # Timer
 obstacle_timer = pygame.USEREVENT + 1
@@ -69,7 +71,7 @@ while True:
                 obstacle_group.add(Obstacles(choice(['eagle'])))
 
         else:
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
                 game_active = True
 
     if game_active:
@@ -85,10 +87,10 @@ while True:
         game_active = collision_sprite()
 
     else:
-        screen.fill((94, 129, 162))
+        screen.fill((44, 141, 222))
         screen.blit(player_stand, player_stand_rect)
 
-        score_message = test_font.render(f'Your score: {score}', False, (111, 196, 169))
+        score_message = test_font.render(f'Your score: {score}', False, (225, 245, 255))
         score_message_rect = score_message.get_rect(center=(400, 330))
         screen.blit(game_name, game_name_rect)
 
--- player.py
+++ player.py
@@ -1,21 +1,27 @@
 import pygame
+
 pygame.init()
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        player_fly_1 = pygame.image.load('graphics/White Bird/up1.png').convert_alpha()
-        player_fly_1 = pygame.transform.rotozoom(player_fly_1, 0, 2)
-        player_fly_2 = pygame.image.load('graphics/White Bird/up2.png').convert_alpha()
-        player_fly_2 = pygame.transform.rotozoom(player_fly_2, 0, 2)
-        player_fly_3 = pygame.image.load('graphics/White Bird/up3.png').convert_alpha()
-        player_fly_3 = pygame.transform.rotozoom(player_fly_3, 0, 2)
+        player_fly_1 = pygame.image.load('graphics/White Bird/tile006.png').convert_alpha()
+        player_fly_1 = pygame.transform.rotozoom(player_fly_1, 180, 2)
+        player_fly_2 = pygame.image.load('graphics/White Bird/tile007.png').convert_alpha()
+        player_fly_2 = pygame.transform.rotozoom(player_fly_2, 180, 2)
+        player_fly_3 = pygame.image.load('graphics/White Bird/tile008.png').convert_alpha()
+        player_fly_3 = pygame.transform.rotozoom(player_fly_3, 180, 2)
         self.player_fly = [player_fly_1, player_fly_2, player_fly_3]
         self.player_index = 0
 
         self.image = self.player_fly[self.player_index]
         self.rect = self.image.get_rect(midbottom=(400, 780))
+
+    def animation_state(self):
+        self.player_index += 0.3
+        if self.player_index >= len(self.player_fly): self.player_index = 0
+        self.image = self.player_fly[int(self.player_index)]
 
     def player_input(self):
         keys = pygame.key.get_pressed()
@@ -24,3 +30,7 @@ class Player(pygame.sprite.Sprite):
                 self.rect.top -= 4
             else:
                 self.rect.top = 780
+
+    def update(self):
+        self.player_input()
+        self.animation_state()
